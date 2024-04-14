43223b6131a17528a21cfb88c19e5cae171ba8b5
him69 <crazybee376@gmail.com>
2024-04-14 21:51:23 +0530

    first comiit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// specifie which engin to use
+app.set('view engine','ejs')
+// base path
+const basePath = 'http://localhost:3000/';
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  let portfolioName = "himanshu"
+  let content =" hi! this is my content"
+  res.render('pages/index',{basePath})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
